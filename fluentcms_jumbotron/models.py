4c28475a13cc3b4c190acca313d73641a2c533f2
graysham <37931277+graysham@users.noreply.github.com>
2021-03-07 21:35:53 +0000

    Update models.py
    changed location of python_2_unicode_compatible import

--- fluentcms_jumbotron/models.py
+++ fluentcms_jumbotron/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
 
